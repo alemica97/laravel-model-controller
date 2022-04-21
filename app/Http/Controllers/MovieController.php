f69c6f6492c84334d346552bad2bd3c1f67b7ceb
alemica97 <alessandro.micalizzi08@gmail.com>
2022-04-21 16:14:57 +0200

    ho creato il metodo Movie, e ho aggiunto un metodo index al controller MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     //
+    // creo un metodo index all'interno del controller 
+    public function index(){
+        
+    }
 }
